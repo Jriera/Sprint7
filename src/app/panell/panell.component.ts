cde9a7916d99a18dd2edc9c5d19f257ed6cfc7c8
Jriera <jordiriera81@gmail.com>
2021-11-03 20:27:32 +0100

    Afegeix reactive forms, i botons de control

--- src/app/panell/panell.component.ts
+++ src/app/panell/panell.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ServeiWeb } from '../models/serveiWeb';
 import { CalculTotalsService } from '../services/calcul-totals.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-panell',
@@ -9,11 +10,17 @@ import { CalculTotalsService } from '../services/calcul-totals.service';
 })
 export class PanellComponent implements OnInit {
   @Input() passedWeb!:ServeiWeb;
+  paginesForm = new FormControl('');
+  idiomesForm = new FormControl('');
 pagines:number =0;
 idiomes:number =0
   constructor(private idiomesPagines:CalculTotalsService) { }
 
   ngOnInit(): void {
+    this.paginesForm.setValue(0);
+    this.idiomesForm.setValue(0);
+
+
   }
 setPagines(){
   this.idiomesPagines.getPagines(this.pagines);
@@ -22,6 +29,22 @@ setIdiomes(){
   this.idiomesPagines.getIdiomes(this.idiomes);
 }
 
+addPagina(){
+  this.paginesForm.setValue(this.paginesForm.value+1)
+
+}
+
+subsPagina(){
+  this.paginesForm.setValue(this.paginesForm.value-1)
+}
+
+addIdioma(){
+  this.idiomesForm.setValue(this.idiomesForm.value+1)
+}
+
+subsIdioma(){
+  this.idiomesForm.setValue(this.idiomesForm.value-1)
+}
 
 
 }
