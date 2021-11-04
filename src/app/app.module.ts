b20b6da2bed17a432c17150c12b6785665033849
Jriera <jordiriera81@gmail.com>
2021-11-04 16:13:31 +0100

    Importa component benvinguda al modul app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PanellComponent } from './panell/panell.component';
+import { BenvingudaComponent } from './benvinguda/benvinguda.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PanellComponent
+    PanellComponent,
+    BenvingudaComponent
   ],
   imports: [
     BrowserModule,
