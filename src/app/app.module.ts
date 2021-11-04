9456fd6a44007ced59bb554ab4f5eeb5528f6592
Jriera <jordiriera81@gmail.com>
2021-11-04 17:56:05 +0100

    Add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PanellComponent } from './panell/panell.component';
 import { BenvingudaComponent } from './benvinguda/benvinguda.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { BenvingudaComponent } from './benvinguda/benvinguda.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
