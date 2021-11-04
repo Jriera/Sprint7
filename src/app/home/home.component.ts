8fc1378f09579b7c203f148f386393e8230abc6c
Jriera <jordiriera81@gmail.com>
2021-11-04 12:10:35 +0100

    Afegeix funcio per actualitzar el preu en cada event capturat

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,58 +6,60 @@ import { CalculTotalsService } from '../services/calcul-totals.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  webCheckboxFlag:boolean=false
-  seoCheckboxFlag:boolean=false
-  adsCheckboxFlag:boolean=false
+  webCheckboxFlag: boolean = false;
+  seoCheckboxFlag: boolean = false;
+  adsCheckboxFlag: boolean = false;
 
-  web:ServeiWeb={
-    nom:'web',
-    preu:500,
-    pagines:0,
-    idiomes:0
+  web: ServeiWeb = {
+    nom: 'web',
+    preu: 500,
+    pagines: 0,
+    idiomes: 0,
+  };
+
+  seo: Servei = {
+    nom: 'seo',
+    preu: 300,
+  };
+
+  ads: Servei = {
+    nom: 'ads',
+    preu: 200,
+  };
+
+  total = 0;
+  constructor(private operativaServeis: CalculTotalsService) {}
+
+  ngOnInit(): void {}
+
+  webSelected() {
+    this.webCheckboxFlag = !this.webCheckboxFlag;
+    this.webCheckboxFlag === true
+      ? this.operativaServeis.addServei(this.web)
+      : this.operativaServeis.removeServei(this.web);
+  }
+  seoSelected() {
+    this.seoCheckboxFlag = !this.seoCheckboxFlag;
+    this.seoCheckboxFlag === true
+      ? this.operativaServeis.addServei(this.seo)
+      : this.operativaServeis.removeServei(this.seo);
+  }
+  adsSelected() {
+    this.adsCheckboxFlag = !this.adsCheckboxFlag;
+    this.adsCheckboxFlag === true
+      ? this.operativaServeis.addServei(this.ads)
+      : this.operativaServeis.removeServei(this.ads);
   }
 
-  seo:Servei={
-    nom:'seo',
-    preu:300
+  getTotal(): void {
+    this.operativaServeis.calculTotal();
+    this.total = this.operativaServeis.totalServeis;
   }
 
-  ads:Servei={
-    nom:'ads',
-    preu:200
+  updateTotal(newTotal: number) {
+    this.total = newTotal;
   }
-
-  total=0;
-  constructor(private operativaServeis:CalculTotalsService) { }
-
-  ngOnInit(): void {
-  }
-  
-
-webSelected(){
-  this.webCheckboxFlag=!this.webCheckboxFlag;
-  this.webCheckboxFlag===true?this.operativaServeis.addServei(this.web):this.operativaServeis.removeServei(this.web)
-  
-}
-seoSelected(){
-  this.seoCheckboxFlag=!this.seoCheckboxFlag;
-  this.seoCheckboxFlag===true?this.operativaServeis.addServei(this.seo):this.operativaServeis.removeServei(this.seo)
- 
-}
-adsSelected(){
-  this.adsCheckboxFlag=!this.adsCheckboxFlag;
-  this.adsCheckboxFlag===true?this.operativaServeis.addServei(this.ads):this.operativaServeis.removeServei(this.ads)
- 
-}
-
-getTotal():void{
-  this.operativaServeis.calculTotal();
-  this.total=this.operativaServeis.totalServeis;
-  
-}
-
-
 }
