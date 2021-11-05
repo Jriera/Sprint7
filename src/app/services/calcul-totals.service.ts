b9e8de2bb1a25d4ecc4bb595f18080d09eb0585d
Jriera <jordiriera81@gmail.com>
2021-11-05 18:43:42 +0100

    feat:Afegeix logica de pressupostos i resets

--- src/app/services/calcul-totals.service.ts
+++ src/app/services/calcul-totals.service.ts
@@ -1,83 +1,97 @@
 import { Injectable } from '@angular/core';
 import { Servei } from '../models/servei';
 import { ServeiWeb } from '../models/serveiWeb';
+import { Pressupost } from '../models/pressupost';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CalculTotalsService {
-serveis:Servei[]=[];
-pagines:number=0;
-idiomes:number=0;
-totalServeis:number=0;
+  serveis: Servei[] = [];
+  pressupost:Pressupost = {
+    nom:'',
+    client:'',
+    serveis:this.serveis
+  }
+  pressupostosList:Pressupost[]=[];
+  pagines: number = 0;
+  idiomes: number = 0;
+  totalServeis: number = 0;
 
-web:ServeiWeb={
-  nom:'web',
-  preu:500,
-  pagines:0,
-  idiomes:0
-}
+  web: ServeiWeb = {
+    nom: 'web',
+    preu: 500,
+    pagines: 0,
+    idiomes: 0,
+  };
 
-seo:Servei={
-  nom:'seo',
-  preu:300
-}
+  seo: Servei = {
+    nom: 'seo',
+    preu: 300,
+  };
 
-ads:Servei={
-  nom:'ads',
-  preu:200
-}
+  ads: Servei = {
+    nom: 'ads',
+    preu: 200,
+  };
 
+  constructor() {}
 
-  constructor() { }
-
-  addServei(servei:Servei):void{
-  this.serveis.push(servei);
+  addServei(servei: Servei): void {
+    this.serveis.push(servei);
   }
 
-  removeServei(servei:Servei):void{
-    const index:number=this.serveis.indexOf(servei)
-    this.serveis.splice(index,1);
-   
-    
-  }
-    
-    
-
-  calculTotal(){
-   this.totalServeis= this.serveis.map(servei=>servei.preu)
-            .reduce((total,s)=>total=total+s,0)
+  removeServei(servei: Servei): void {
+    const index: number = this.serveis.indexOf(servei);
+    this.serveis.splice(index, 1);
   }
 
-  calculPreuWeb(web:ServeiWeb){
-    const preuWebInicial=500;
-    web.preu= preuWebInicial + (web.pagines*web.idiomes*30)
+  calculTotal() {
+    this.totalServeis = this.serveis
+      .map((servei) => servei.preu)
+      .reduce((total, s) => (total = total + s), 0);
   }
 
-  getPagines(pagines:number):void{
-    this.pagines=pagines;
-    
-
-  }
-  getIdiomes(idiomes:number):void{
-    this.idiomes=idiomes;
-    
+  calculPreuWeb(web: ServeiWeb) {
+    const preuWebInicial = 500;
+    web.preu = preuWebInicial + web.pagines * web.idiomes * 30;
   }
 
-  updateServeiWeb(){
-    this.web.pagines=this.pagines;
-   this.web.idiomes=this.idiomes;
+  getPagines(pagines: number): void {
+    this.pagines = pagines;
+  }
+  getIdiomes(idiomes: number): void {
+    this.idiomes = idiomes;
+  }
+
+  updateServeiWeb() {
+    this.web.pagines = this.pagines;
+    this.web.idiomes = this.idiomes;
     this.calculPreuWeb(this.web);
     this.addServei(this.web);
-    console.log(this.serveis)
+    console.log(this.serveis);
   }
 
-  resetServeiWeb(){
-    this.web.preu=500
-    this.web.pagines=0
-    this.web.pagines=0
+  resetServeiWeb() {
+    this.web.preu = 500;
+    this.web.pagines = 0;
+    this.web.pagines = 0;
   }
-   
 
+  creaPressupost(nom:string,client:string){
+    this.pressupost.nom=nom;
+    this.pressupost.client=client;
+    this.pressupost.serveis = this.serveis
+  }
 
+  addPressupost(){
+    this.pressupostosList.push(this.pressupost) 
+    console.log(this.pressupostosList);
+  }
+
+  resetServices(){
+    this.resetServeiWeb();
+    this.serveis=[];
+    this.totalServeis=0;
+  }
 }
