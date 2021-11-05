dbaafa521dd890fcf3a4a7090e846a11899ffccc
Jriera <jordiriera81@gmail.com>
2021-11-05 21:09:01 +0100

    feat:Crea classe Pressupost

--- src/app/models/pressupost.ts
+++ src/app/models/pressupost.ts
@@ -1,8 +1,18 @@
 import { Servei } from "./servei";
 
-export interface Pressupost{
+export class Pressupost{
     nom:string;
     client:string;
     serveis:Servei[];
+    preu:number;
+    data:String;
+
+    constructor(nom:string, client:string, serveis:Servei[], preu:number, data:String){
+        this.nom = nom;
+        this.client = client;
+        this.serveis = serveis;
+        this.preu = preu;
+        this.data = data;
+    }
 }
 
